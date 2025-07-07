358c2ae5812e0c0946a0f30d3de9935d52d0dc61
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-07 15:12:23 +0300

    day2: true/false, Relational operators in C, Logical operators in C, Conditional Statements, loops, for, while, do while

--- C/day2.c
+++ C/day2.c
@@ -1,41 +1,275 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 void main(void){
 	
 	
 	
-	/*
-	// lab2
-	char grade;
-	printf("Please, enter your grade (0 to 100): ");
-	scanf("%d",&grade);
+
 	
-	if(grade>0 && grade<50)
-		printf("Failed!");
-	else if(grade>=50 && grade<65)
-		printf("Pass!");
-	else if(grade>=65 && grade<75)
-		printf("Good!");
-	else if(grade>=75 && grade<85)
-		printf("Very Good!");
-	else if(grade>=85)
-		printf("Excellent!");
+	/*// lab
+	int num=0;
+	
+	while(1){
+		printf("Enter you val: ");
+		scanf("%d", &num);
+		
+		if(num > 100)
+		break;
+	}*/
+	
+	
+	
+	
+	/*// lab: fac
+	
+	int num=0, fac=1;
+	
+	printf("Enter your number: ");
+	scanf("%d",&num);
+	
+	while(num>1){
+		fac *= num;
+		num--;
+	}
+	
+	printf("Your fac is %d\n", fac);
 	*/
 	
-	/*
-	// lab1
-	char userAge;
-	printf("Please enter your age: ");
-	scanf("%d",&userAge);
 	
-	if(userAge >= 16){
-		printf("Yes, you can drive\n");
-	}else{
-		printf("Sorry, you can\'t\n");
+	
+	
+	
+	
+	/*// lab
+	int num=0;
+
+	do{
+		
+	printf("PLease enter a number < 100: ");
+	scanf("%d",&num);
+	
+	}while(num>100);*/
+	
+	/*// lab
+	int num=0;
+	printf("PLease enter a number < 100: ");
+	scanf("%d",&num);
+	
+	while(num>100){
+	printf("PLease enter a number < 100: ");
+	scanf("%d",&num);
+	}*/
+	
+	
+	
+	
+	
+	/*
+	char num=0, sum=0;
+	for(char c=0;c<10;++c){
+		printf("Enter your (%d): ",c);
+		scanf("%d", &num);
+		
+		sum += num;
+	}
+	
+	printf("Sum is %d\n", sum);*/
+	
+	
+	/*
+	
+	for(char i=0;i<=20;i+=1){
+		if(i%2 == 0){
+			printf("%d is even\n",i);
+		}else{
+			printf("%d is odd\n",i);
+		}
+	}
+	
+	*/
+	
+	
+	
+	
+	
+	/*// lab
+	int id;
+	printf("Enter your ID: ");
+	scanf("%d",&id);
+	
+	
+	switch(id){
+		case 1234: 
+		printf("P123!\n");		
+		break;
+		case 5677: 
+		printf("P567!\n");		
+		break;
+		case 1145:
+		printf("P114!\n");		
+		break;
+		default:
+		printf("Not found!\n");
 	}
 	*/
+	
+
+
+	
+	
+	
+	
+	
+	/*// lab
+	char grade = 'D';
+	switch(grade){
+		case 'A':
+		printf("Good\n");
+		break;
+		case 'B':
+		printf("Pass\n");
+		break;
+		case 'F':
+		printf("Faild\n");
+		break;
+		
+	}
+	*/
+	
+	
+	
+	
+	
+	
+	/*
+	char year, isSw, isMath;
+	printf("Enter isxyear: ");
+	scanf("%d",&year);
+	
+	printf("Enter isMath: ");
+	scanf("%d",&isMath);
+	
+	printf("Enter isSw: ");
+	scanf("%d",&isSw);
+	
+	
+	
+	if(year == 1){
+		
+		if(isMath){
+			printf("Math1\n");
+		}else if(isSw){
+			printf("SW1\n");
+			
+		}else{
+			printf("Nothing\n!");
+		}
+		
+	}else if(year == 2){
+		
+		if(isMath){
+			printf("Math2\n");
+		}else if(isSw){
+			printf("SW2\n");
+			
+		}else{
+			printf("Nothing\n!");
+		}
+		
+	}else{
+		printf("Nothing\n!");
+	}
+	*/
+	
+	
+	/*
+	if(cond){
+		.
+		.
+		.
+		.
+	}else{
+		.
+		.
+		.
+		.
+	}
+	
+	*/
+	
+	
+	
+	/*// lab2
+	char grade;
+	printf("PLease, enter your grade (0 to 100): ");
+	scanf("%d",&grade);
+	
+	if((grade>=0) && (grade<50)){
+		printf("Fail\n");
+	}else if((grade>=50) && (grade<65)){
+		printf("Pass\n");
+	}else if((grade>=65) && (grade<75)){
+		printf("Good\n");
+	}else if((grade>=75) && (grade<85)){
+		printf("Very Good\n");
+	}else if(grade>=85){
+		printf("Excellent\n");
+	}else{
+		printf("Wrong input!");
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*// lab2
+	int age;
+	printf("Enter your age: ");
+	scanf("%d",&age);
+	
+	if(age > 16){
+		printf("Yes, you can\n");
+	}else{
+		printf("No, you can\'t\n");
+	}*/
+	
+	
+	
+	
+	/*
+	// lab1: GCC see true as 1, false as 0
+	printf("%d", 3==3);
+	*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
