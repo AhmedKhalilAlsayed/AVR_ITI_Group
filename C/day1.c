ebd2f781303ae163c0cc6e51110285a19a3726ef
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-07 03:49:53 +0300

    day1: printf, operators, scanf, variables

---
+++ C/day1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main(void){
+	
+	/* day1 6-7-25 labs: swap, get/set bit */
+	// test print function
+	/*
+	int num = 1000;
+	char grade = 'A';
+	float salary = 15.15;
+	
+	printf("num = %d\n", num);
+	printf("grade = %c %d\n", grade,grade);
+	printf("salary = %f\n", salary);*/
+	
+	
+	// size of datatype
+	/*
+	printf("char\t%d\n",sizeof(char));
+	printf("int\t%d\n",sizeof(int));
+	printf("float\t%d\n",sizeof(float));
+	printf("double\t%d\n",sizeof(double));*/
+	
+	
+	// test scanf
+	/*int gpa;
+	char grade;
+	
+	printf("Enter your gpa (I): ");
+	scanf("%d",&gpa);
+	
+	printf("Enter your grade (C): ");
+	scanf("\n%c",&grade);
+	
+	
+	printf("echo: %d\n", gpa);
+	printf("echo: %c", grade);
+	*/
+	
+	
+	
+	
+	
+	/*
+	float num1, num2;
+	// take 2 values
+	printf("Enter number 1 (float): ");
+	scanf("%f",&num1);
+	printf("Enter number 2 (float): ");
+	scanf("%f",&num2);
+	
+	// opeartaions
+	printf("sum = %.0f\n", num1+num2);
+	printf("sub = %f\n", num1-num2);
+	printf("div = %f\n", num1/num2);*/
+	
+	/* bitwise ...
+	int num1, num2;
+	// take 2 values
+	printf("Enter number 1 (int): ");
+	scanf("%d",&num1);
+	printf("Enter number 2 (int): ");
+	scanf("%d",&num2);
+	
+	// opeartaions
+	printf("AND = %d\n", num1&num2);
+	printf("OR  = %d\n", num1|num2);
+	printf("XOR = %d\n", num1^num2);
+	*/
+	
+	
+	
+	
+	
+}
---
+++ C/day2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main(void){
+	char grade = 'A';
+	switch(grade){
+		case 'A': cout << "Hi" << endl; break;
+	}
+}
