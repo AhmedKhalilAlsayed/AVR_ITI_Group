727bfef6a9c60cc761151edb96fa62b050c94754
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-10 10:30:29 +0300

    sizeof, short, long

--- C/day5/day5.c
+++ C/day5/day5.c
@@ -1,23 +1,17 @@
-
-void func(int* ptr){
-	
-	if(ptr != NULL){
-		
-		
-		printf("%d", *ptr);
-		
-	}else{
-		
-		
-		
-	}
-	
-	
-	
-}
+#include <stdio.h>
 
 
 void main(){
 	
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(short int));
+	printf("%d\n", sizeof(long int));
+	printf("%d\n\n", sizeof(long long int));
+	
+	printf("%d\n", sizeof(double));
+	printf("%d\n", sizeof(long double));
+	
+	
 	
 }
+
