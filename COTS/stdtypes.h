d98861a6bc4f021d2d841704078b8703b4bd0f7d
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-10 13:55:41 +0300

    COTS/stdtypes.h

---
+++ COTS/stdtypes.h
@@ -0,0 +1,11 @@
+
+// new data types
+
+typedef unsigned char u8;
+typedef signed char s8; 
+typedef unsigned short int u16;
+typedef signed short int s16;
+typedef unsigned int u32;
+typedef signed int s32;
+typedef unsigned long int u64;
+typedef signed long int s64;
