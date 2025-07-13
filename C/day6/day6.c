884935bc9cabeaf993827add316c66a3d1ce3559
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-13 11:51:57 +0300

    typedef struct Employee

--- C/day6/day6.c
+++ C/day6/day6.c
@@ -2,14 +2,14 @@
 #include "stdtypes.h"
 
 
-struct Employee{
+typedef struct Employee{
 	u8 name[15];
 	u32 salary;
 	u8 bonus;
-};
+}Employee;
 
 void main(void){
-	struct Employee arr[3];
+	Employee arr[3];
 	
 	
 	for(u8 i =0;i<3;i++){
