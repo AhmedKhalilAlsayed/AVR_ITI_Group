c175d7f25d733365bf214d960bfff935731f2b2d
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-14 06:55:08 +0300

    union (Byte_t) example with struct (struct bits)

--- C/day6/day6.c
+++ C/day6/day6.c
@@ -3,17 +3,29 @@
 #include "stdtypes.h"
 
 
+// a new data type (union) to define a byte value and its bits with struct
 
-typedef struct Student{
-	u8 x;
-	u16 z;
-	u32 y;
-}Student;
+typedef union Byte_t{
+	u8 byteValue;
+	struct bits{
+		u8 b0 : 1;
+		u8 b1 : 1;
+		u8 b2 : 1;
+		u8 b3 : 1;
+		u8 b4 : 1;
+		u8 b5 : 1;
+		u8 b6 : 1;
+		u8 b7 : 1;
+	};
+}Byte_t;
 
 
 void main(void){
-	Student s;
-	printf("%d", sizeof(s));
 	
+	Byte_t motorControl ={255};
+	
+	printf("byteValue = %d\n", motorControl.byteValue);
+	
+	printf("in bits = %d%d%d%d %d%d%d%d\n", motorControl.b7, motorControl.b6, motorControl.b5,motorControl.b4,motorControl.b3,motorControl.b2,motorControl.b1,motorControl.b0);
 	
 }
