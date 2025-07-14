a24a8b0f20a0df7c971aa96660c70795f6d1c4af
Ahmed Khalil <ahmedkhalilalsayed@gmail.com>
2025-07-14 07:01:25 +0300

    enum example with switch case

--- C/day6/day6.c
+++ C/day6/day6.c
@@ -3,31 +3,29 @@
 #include "stdtypes.h"
 
 
-// a new data type (union) to define a byte value and its bits with struct
+// new data type that has some persons in enum
 
-typedef union Byte_t{
-	u8 byteValue;
-	struct bits{
-		u8 b0 : 1;
-		u8 b1 : 1;
-		u8 b2 : 1;
-		u8 b3 : 1;
-		u8 b4 : 1;
-		u8 b5 : 1;
-		u8 b6 : 1;
-		u8 b7 : 1;
-	};
-}Byte_t;
-
+typedef enum{
+	Ahmed,
+	Ali,
+	Mohammed
+}Persons;
 
 void main(void){
 	
-	Byte_t motorControl ={255};
+	//Persons person = 0; //== Ahmed
+	//Persons person = 1; //== Ali
+	//Persons person = 2; //== Mohammed
 	
-	printf("byteValue = %d\n", motorControl.byteValue);
+	//Persons person = Ahmed;	// == 0 
+	Persons person = Ali; 		//== 1
+	//Persons person = Mohammed; //== 2
 	
-	printf("in bits = %d%d%d%d %d%d%d%d\n", motorControl.b7, motorControl.b6, motorControl.b5,motorControl.b4,motorControl.b3,motorControl.b2,motorControl.b1,motorControl.b0);
+	switch(person){
+		case Ahmed: printf("Data of Ahmed\n"); break;
+		case Ali: printf("Data of Ali\n"); break;
+		case Mohammed: printf("Data of Mohammed\n"); break;
+	}
 	
 	
-	printf("sizeof %d", sizeof(motorControl));
 }
